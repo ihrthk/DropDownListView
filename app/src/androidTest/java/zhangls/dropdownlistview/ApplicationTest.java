e4f346c5df71bfb8f2ad6f4bf34c552003201e03
zhangls <ihrthk@gmail.com>
2014-12-29 15:45:38 +0800

    Initial commit

---
+++ app/src/androidTest/java/zhangls/dropdownlistview/ApplicationTest.java
@@ -0,0 +1,13 @@
+package zhangls.dropdownlistview;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://drop_down_listview_demo.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
